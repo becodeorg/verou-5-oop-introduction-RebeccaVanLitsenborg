8a069298d3de09131316618532b7caded9cd70fb
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-01-11 11:35:21 +0100

    Make a getInfo function

---
+++ exercise1.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+class Beverage
+{
+    public string $color;
+    public float $price;
+    public string $temperature;
+
+    public function __construct(string $color, float $price, string $temperature = 'cold')
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo()
+    {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+}
+
+
+/* EXERCISE 1
+
+Remember for now we will use properties and methods that can be accessed from everywhere.
+TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+ print the getInfo on the screen.
+TODO: Print the temperature on the screen.
+
+USE TYPEHINTING EVERYWHERE!
+*/
