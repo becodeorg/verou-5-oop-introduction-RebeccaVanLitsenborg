d31c5e57da270acc6591de1f0d69f0fc0b5e2d69
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-01-11 12:00:07 +0100

    Instantiate an object which represents cola and print temperature

--- exercise1.php
+++ exercise1.php
@@ -15,17 +15,19 @@ class Beverage
         $this->temperature = $temperature;
     }
 
-    public function getInfo()
+    public function getInfo(): string
     {
         return "This beverage is {$this->temperature} and {$this->color}.";
     }
 }
 
+$cola = new Beverage("black", 2.0);
+echo $cola->getInfo() . "\n";
+echo "Temperature: {$cola->temperature}";
+
 
 /* EXERCISE 1
 
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
 TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
  print the getInfo on the screen.
 TODO: Print the temperature on the screen.
