55c3605e7752be60e9308c7513cf5a429603dec5
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-01-12 09:30:46 +0100

    Make properties private

--- exercise3.php
+++ exercise3.php
@@ -1,21 +1,18 @@
 <?php
 
 declare(strict_types=1);
+/* EXERCISE 3
 
-/* EXERCISE 2
+TODO: Copy the code of exercise 2 to here and delete everything related to cola.
+TODO: Make all properties private.
+TODO: Make all the other prints work without error.
+TODO: After fixing the errors. Change the color of Duvel to light instead of blond and also print this new color on the screen after all the other things that were already printed (to be sure that the color has changed).
+TODO: Create a new private method in the Beer class called beerInfo which returns "Hi i'm Duvel and have an alcochol percentage of 8.5 and I have a light color."
 
-TODO: Make class beer that extends from Beverage.
-TODO: Create the properties name (string) and alcoholPercentage (float).
-TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholpercentage.
+Make sure that you use the variables and not just this text line.
 
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getAlcoholPercentage function which returns the alocoholPercentage.
-TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
-TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
-TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+TODO: Print this method on the screen on a new line.
 
-Make sure that each print is on a different line.
-Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
 USE TYPEHINTING EVERYWHERE!
 */
 
