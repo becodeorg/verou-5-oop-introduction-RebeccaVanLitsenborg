3fd23f57b474def442dd3b352da9e38b8b5acfc8
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-01-14 13:22:43 +0100

    Change all properties to protected

---
+++ exercise4.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/* EXERCISE 4
+
+Copy the code of exercise 2 to here and delete everything related to cola.
+
+TODO: Make all properties protected.
+TODO: Make all the other prints work without error without changing the beverage class.
+
+USE TYPEHINTING EVERYWHERE!
+*/
+
+class Beverage
+{
+    protected string $color;
+    protected float $price;
+    protected string $temperature;
+
+    public function __construct(string $color, float $price, string $temperature = 'cold')
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+    public function getInfo(): string
+    {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+}
+
+class Beer extends Beverage
+{
+    protected string $name;
+    protected float $alcoholPercentage;
+
+
+    public function __construct(string $name, float $alcoholPercentage, string $color, float $price, string $temperature ='cold')
+    {
+        parent::__construct($color, $price, $temperature);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    public function getAlcoholPercentage() 
+    {
+        return $this->alcoholPercentage;
+    }
+}
+
+$duvel = new Beer('Duvel', 8.5, 'blond', 3.5);
+echo $duvel->getAlcoholPercentage() . "<br>";
+echo $duvel->alcoholPercentage . "<br>";
+echo $duvel->getInfo() . "<br>";
+
+
+$duvel = new Beverage ('Duvel', 8.5, 'blond', 3.5);
+echo $duvel->getAlcoholPercentage() . "<br>";
